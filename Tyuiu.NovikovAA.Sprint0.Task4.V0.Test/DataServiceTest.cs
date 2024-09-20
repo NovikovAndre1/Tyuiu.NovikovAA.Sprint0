1b4f93d9c2bc571a29d096f052bb7a81d9d8580f
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-09-20 23:49:06 +0600

    Task4

---
+++ Tyuiu.NovikovAA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,42 @@
+using System.Security.Cryptography.X509Certificates;
+using Tyuiu.NovikovAA.Sprint0.Task4.V0.Lib;
+
+
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void CheckedAdditionValid()
+    {
+        Assert.AreEqual(10, DataService.Addition(5, 5));
+    }
+
+    [TestMethod]
+
+    public void CheckedSubtractionValid()
+    {
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+    }
+
+    [TestMethod]
+
+    public void CheckedDivisionValid()
+    {
+        Assert.AreEqual(3, DataService.Division(9, 3));
+    }
+
+    [TestMethod]
+
+    public void CheckedMultiplicationValid()
+    {
+        Assert.AreEqual(50, DataService.Multiplication(10, 5));
+    }
+}
+    
+  
+        
+
+
